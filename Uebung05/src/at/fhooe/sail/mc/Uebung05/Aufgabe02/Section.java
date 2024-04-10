e123be20c65b057c5085a7f6fc0522758da88ff6
Haris <hariskekic@hotmail.com>
2024-04-10 20:22:15 +0200

    getIncome methode in section überschrieben, funktionalität unklar

--- Uebung05/src/at/fhooe/sail/mc/Uebung05/Aufgabe02/Section.java
+++ Uebung05/src/at/fhooe/sail/mc/Uebung05/Aufgabe02/Section.java
@@ -2,30 +2,38 @@ package at.fhooe.sail.mc.Uebung05.Aufgabe02;
 
 import at.fhooe.sail.mc.Uebung05.Aufgabe01.*;
 
-public class Section extends AbstractMember{
-	
+public class Section extends AbstractMember {
+
 	private BinarySearchTree tree = new BinarySearchTree();
 
 	public Section(String name) {
 		super(name);
-		// TODO Auto-generated constructor stub
+	}
+
+	boolean addMember(AbstractMember m) {
+		return tree.insert(m);
+	}
+
+	boolean removeMember(AbstractMember m) {
+		return tree.remove(m);
+	}
+
+	boolean isMember(AbstractMember m) {
+		return tree.find(m);
 	}
 
 	@Override
 	public double getIncome() {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public double getCost() {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public double getSurplus() {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
